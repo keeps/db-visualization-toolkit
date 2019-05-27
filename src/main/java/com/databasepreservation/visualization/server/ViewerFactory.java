928104b1bcfa0d77d898a0f918744a9cd3356650
Luis Faria <lfaria@keep.pt>
2019-05-27 18:20:14 +0100

    Fixing ViewerFactory concurrency, adding Electron readme

--- src/main/java/com/databasepreservation/visualization/server/ViewerFactory.java
+++ src/main/java/com/databasepreservation/visualization/server/ViewerFactory.java
@@ -51,10 +51,12 @@ public class ViewerFactory {
   private static ViewerConfiguration configuration;
   private static boolean instantiated = false;
 
-  private static void instantiate() {
-    configuration = ViewerConfiguration.getInstance();
-    instantiateSolrCloud();
-    instantiated = true;
+  private static synchronized void instantiate() {
+    if(!instantiated) {
+      configuration = ViewerConfiguration.getInstance();
+      instantiateSolrCloud();
+      instantiated = true;
+    }
   }
 
   private static void instantiateSolrCloud() {
@@ -274,23 +276,17 @@ public class ViewerFactory {
   }
 
   public static SolrManager getSolrManager() {
-    if (!instantiated) {
-      instantiate();
-    }
+    instantiate();
     return solrManager;
   }
 
   public static CloudSolrClient getSolrClient() {
-    if (!instantiated) {
-      instantiate();
-    }
+    instantiate();
     return cloudSolrClient;
   }
 
   public static ViewerConfiguration getViewerConfiguration() {
-    if (!instantiated) {
-      instantiate();
-    }
+    instantiate();
     return configuration;
   }
 }
